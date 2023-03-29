96a3a3c563dff2ee47005fc2fb0631cf08ee27be
Null_Delta <delta.null@vk.com>
2023-03-29 16:52:18 +0300

    make right code

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,8 @@
 #include<iostream>
 #include<string>
 
-using namespace std;
-
 int main() {
 	string name;
-	getLine(cin, name);
-	cout << "Hello World from " << name <<  endl;
+	getLine(std::cin, name);
+	std::out << "Hello World from " << name <<  std::endl;
 }
